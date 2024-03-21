dea6b0ed196438837b2f2b4dac2ec2d42c480e06
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2024-03-21 22:26:32 +0530

    feat(env): setup env config with validation in server

---
+++ server/src/config/config.options.ts
@@ -0,0 +1,30 @@
+import { ConfigModuleOptions } from '@nestjs/config'
+import * as joi from 'types-joi'
+import { InterfaceFrom } from 'types-joi'
+
+export enum NodeEnv {
+  Production = 'production',
+  Staging = 'staging',
+  Development = 'development',
+}
+
+const validationSchema = joi
+  .object({
+    NODE_ENV: joi
+      .string()
+      .valid(...Object.values(NodeEnv))
+      .required(),
+    PORT: joi.number().integer().required(),
+    DATABASE_URL: joi.string().required(),
+  })
+  .required()
+
+export type Environment = InterfaceFrom<typeof validationSchema>
+
+const configOptions: ConfigModuleOptions = {
+  envFilePath: ['.env', '.env.development', '.env.production'],
+  isGlobal: true,
+  validationSchema,
+}
+
+export default configOptions
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core'
+import { ConfigService } from '@nestjs/config'
+import { ValidationPipe } from '@nestjs/common'
+import { NestExpressApplication } from '@nestjs/platform-express'
 import { AppModule } from './app.module'
+import { Environment } from './config/config.options'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(8000)
+  /** Creating application */
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+
+  /** Our environment config service */
+  const configService: ConfigService<Environment> = app.get(ConfigService)
+
+  /** Enabling CORS */
+  app.enableCors({ origin: '*', exposedHeaders: 'content-disposition' })
+
+  /** Enabling validation in app */
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
+
+  const port = configService.get<number>('PORT')
+
+  await app.listen(port)
 }
 bootstrap()
