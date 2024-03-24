268391b7fa8ebfe02b2afa45a4f01ae913bf4384
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2024-03-24 12:04:29 +0530

    feat: add button in app to show shadcn is working

--- client/src/App.tsx
+++
@@ -1,3 +0,0 @@
-export default function App() {
-  return <div className="flex h-screen items-center justify-center text-9xl font-extrabold">Handy Hero</div>
-}
--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,3 +1,11 @@
+import { Button } from '@/components/ui/button'
+
 export default function App() {
-  return <div className="flex h-screen items-center justify-center text-9xl font-extrabold">Handy Hero</div>
+  return (
+    <div className="flex h-screen items-center justify-center text-9xl font-extrabold flex-col gap-4">
+      <h1>Handy Hero</h1>
+
+      <Button>Shadcn Works</Button>
+    </div>
+  )
 }
