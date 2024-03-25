f02ea256557c5a8ead43f09b0d128fe86de3f9e0
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2024-03-25 09:27:15 +0530

    refactor(role-guard): update type definition in Role decorator

--- server/src/auth/guards/role.decorator.ts
+++ server/src/auth/guards/role.decorator.ts
@@ -1,3 +1,4 @@
 import { Reflector } from '@nestjs/core'
+import { Role as RoleEnum } from '@prisma/client'
 
-export const Role = Reflector.createDecorator<'CLIENT' | 'SERVICE_PROVIDER'>()
+export const Role = Reflector.createDecorator<RoleEnum>()
