f4c19a0510e901cfa4c1c7326ae561b1144962a8
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2024-03-25 21:49:25 +0530

    feat(profile): create service provider profile schema

--- server/src/users/users.dto.ts
+++ server/src/users/users.dto.ts
@@ -9,6 +9,14 @@ export class CreateUserDto {
   @IsEmail()
   email: string
 
+  /** @TODO : Figure out validation for mobile number for different countries */
+  @IsString()
+  mobileNumber: string
+
+  @IsString()
+  @MinLength(2)
+  country: string
+
   @IsEnum(Role)
   role: Role
 
