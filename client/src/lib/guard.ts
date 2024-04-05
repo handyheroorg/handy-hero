72135f56b62faf655a07216353b541de27697cf6
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2024-04-05 22:02:13 +0530

    feat(authorization): create loaders to protect routes according to roles

---
+++ client/src/lib/guard.ts
@@ -0,0 +1,17 @@
+import { redirect } from 'react-router-dom'
+import { fetchLoggedInUser } from '@/queries'
+import { Role } from '@/types'
+
+export async function roleGuard(allowedRole: Role[]) {
+  const data = await fetchLoggedInUser()
+
+  if (!data) {
+    return redirect('/auth/login')
+  }
+
+  if (!allowedRole.includes(data.role)) {
+    return redirect('/')
+  }
+
+  return null
+}
