a87ec3b76f2416de47ec917df52dee7f348e9927
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2024-04-09 20:11:07 +0530

    refactor(dashboard): add create service button on dashboard

--- client/src/pages/provider-dashboard/provider-dashboard.tsx
+++ client/src/pages/provider-dashboard/provider-dashboard.tsx
@@ -1,7 +1,9 @@
-import { MailIcon, PhoneIcon } from 'lucide-react'
+import { MailIcon, PhoneIcon, PlusIcon } from 'lucide-react'
+import { Link } from 'react-router-dom'
 import Container from '@/components/container'
 import VerifyCard from '@/components/verify-card'
 import { useAuthenticatedUser } from '@/hooks'
+import { Button } from '@/components/ui'
 
 export default function ProviderDashboard() {
   const { user } = useAuthenticatedUser()
@@ -9,6 +11,14 @@ export default function ProviderDashboard() {
   return (
     <Container className="!py-8">
       <h1 className="text-2xl font-bold mb-4">{user.fullName.split(' ')[0]}&apos;s Workspace</h1>
+
+      <div className="flex items-center justify-end">
+        <Link to="/services/create">
+          <Button icon={<PlusIcon />} className="mb-4">
+            Add Service
+          </Button>
+        </Link>
+      </div>
 
       <div className="flex items-center gap-4 flex-col md:flex-row">
         <VerifyCard
