e1d7a579aa3a276454480f3032f39c3e5e538ed6
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2024-04-18 10:24:18 +0530

    refactor(chat-request): update button label

--- client/src/pages/services/components/create-chat-request-button.tsx
+++ client/src/pages/services/components/create-chat-request-button.tsx
@@ -48,7 +48,7 @@ export default function CreateChatRequestButton({
         createChatRequestMutation.mutate(serviceId)
       }}
     >
-      Create Chat Request
+      Send Chat Request
     </Button>
   )
 }
